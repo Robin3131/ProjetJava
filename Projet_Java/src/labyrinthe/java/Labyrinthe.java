9e08635675d0671f615b97ea7101013103b3fcfb
Leni Roussel <leniroussel@gmail.com>
2021-04-09 11:14:46 +0200

    test

--- Projet_Java/src/labyrinthe/java/Labyrinthe.java
+++ Projet_Java/src/labyrinthe/java/Labyrinthe.java
@@ -34,73 +34,77 @@ public Labyrinthe() {
 	Piece P24 = new Piece();
 	Piece P25 = new Piece();
 	
-	//P1
-    P1.setSud(P8);
-    P1.setOuest(P2);
-
-    //P2
+	P1.setOuest(P2);
     P2.setEst(P1);
+    
+    P1.setSud(P8);
+    P8.setNord(P1);
+    
+    P8.setOuest(P9);
+    P9.setEst(P8);
+    
+    P9.setSud(P24);
+	P24.setNord(P9);
 
-    //P11
-    P11.setNord(P12);
-    P11.setSud(P10);
-
-    //P6
-    P6.setNord(P5);
-    P6.setEst(P19);
-    P6.setSud(P7);
-
-    //P19
-    P19.setSud(P20);
-    P19.setOuest(P6);
+	P24.setOuest(P25);
+	P25.setEst(P24);
 	
-	//P25
 	P25.setNord(P10);
-    P25.setEst(P24);
-    //P24
-    P24.setOuest(P25);
-    P24.setNord(P9);
-    //P23
-    P23.setEst(P22);
-    //P22
-    P22.setOuest(P23);
-    P22.setNord(P7);
-    //P21
-    P21.setNord(P20);
-    //P12
-    P12.setNord(P13);
-    P12.setSud(P11);
-    //P3
-    P3.setNord(P14);
-    P3.setEst(P4);
-    //P4
-    P4.setOuest(P3);
-    //P5
-    P5.setNord(P16);
-    P5.setEst(P18);
-    P5.setSud(P6);
-    //P18
-    P18.setNord(P17);
-	
-	//P7
-	P7.setSud(P22);
-	P7.setNord(P6);
-	
-	//P8
-	P8.setOuest(P9);
-	P8.setNord(P1);
-	
-	//P9
-	P9.setSud(P24);
-	P9.setEst(P8);
-	
-	//P10
-	P10.setNord(P11);
 	P10.setSud(P25);
 	
-	//P20
-	P20.setSud(P21);
+	P10.setNord(P11);
+	P11.setSud(P10);
+	
+	P11.setNord(P12);
+	P12.setSud(P11);
+   
+	P12.setNord(P13);
+	P13.setSud(P12);
+
+	P13.setEst(P14);	
+	P14.setOuest(P13);
+	
+	P14.setSud(P3);
+	P3.setNord(P14);
+	
+	P3.setEst(P4);
+	P4.setOuest(P3);
+	
+	P4.setNord(P15);
+	P15.setSud(P4);
+	
+	P15.setEst(P16);
+	P16.setOuest(P15);
+	
+	P16.setSud(P5);
+	P5.setNord(P16);
+	
+	P5.setEst(P18);
+	P18.setOuest(P5);
+	
+	P18.setNord(P17);
+	P17.setSud(P18);
+	
+	P5.setSud(P6);
+	P6.setNord(P5);
+	
+	P6.setEst(P19);
+	P19.setOuest(P6);
+	
+	P19.setSud(P20);
 	P20.setNord(P19);
+	
+	P20.setSud(P21);
+	P21.setNord(P20);
+   
+	P6.setSud(P7);
+	P7.setNord(P6);
+	
+	P7.setSud(P22);
+	P22.setNord(P7);
+	
+	P22.setOuest(P23);
+	P23.setEst(P22);
 			
 	
 }
