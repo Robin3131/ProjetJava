2cbb9d7f326e5761d2d985f2d0f3de1c4aea788a
Leni Roussel <leniroussel@gmail.com>
2021-04-13 14:34:40 +0200

    test combat

--- Projet_Java/src/test/Test_combat.java
+++ Projet_Java/src/test/Test_combat.java
@@ -1,17 +1,35 @@
 package test;
 
 import affrontement.Combat;
+import labyrinthe.java.Labyrinthe;
+import personnage.Heros;
+import personnage.Monstre;
 
 public class Test_combat {
 	public static void main(String[] args) {
 		int test;
 		Combat testC = new Combat();
-		
+		Labyrinthe lab = new Labyrinthe();
+		Heros mec = new Heros();
 		for(int i=0;i<10;i++) {
 			test = testC.choixAttaquant();
 			System.out.println(test);
 		}
-		
+		//combat commence
+		Monstre monstr = new Monstre();
+		boolean fuir;
+		fuir = false;
+		while(!fuir){
+			// voulez vous fuir ?
+			int num = testC.choixAttaquant();
+			if(num== 1) {
+				mec.Attaquer(monstr);
+			}else {
+				monstr.Attaquer(mec);
+			}
+			
+
+		}
 		
 }
 }
