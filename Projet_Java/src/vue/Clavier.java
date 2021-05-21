e6782bd10c798ef3b269c4edbaf8f33ae16210f5
Leni Roussel <leniroussel@gmail.com>
2021-05-21 14:53:14 +0200

    commentaire sur vue

--- Projet_Java/src/vue/Clavier.java
+++ Projet_Java/src/vue/Clavier.java
@@ -2,14 +2,22 @@ package vue;
 
 import java.util.Scanner;
 
+
 public class Clavier {
 	    private static Scanner scanner = new Scanner(System.in);
 
-
+	    /**
+	     *Fonction qui retourne un string entré par l'utilisateur
+	     *@return string
+	     */
 	    public static String entrerClavierString() {
 	        return scanner.next();
 	    }
 	    
+	    /**
+	     *Fonction qui retourne un nombre entré par l'utilisateur
+	     *@return int
+	     */
 	    public static int entrerClavierInt() {
 	        return scanner.nextInt();
 	    }
